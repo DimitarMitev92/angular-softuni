dddfa6ef7621b922d489093882c346a8336ed307
Dimitar Mitev <d.mitev920123@gmail.com>
2023-06-20 23:49:22 +0300

    02. Tickets

---
+++ 01. Intro to Angular and TypeScript/02. Tickets/index.ts
@@ -0,0 +1,51 @@
+class Ticket {
+    destination: string;
+    price: number;
+    status: string;
+
+    constructor(destination: string, price: number, status: string) {
+        this.destination = destination;
+        this.price = price;
+        this.status = status;
+    }
+}
+
+function tickets(
+    array: string[],
+    criteria: string
+): { destination: string; price: number; status: string }[] {
+    const destinations: {
+        destination: string;
+        price: number;
+        status: string;
+    }[] = array
+        .map((string) => string.split('|'))
+        .map(
+            ([destination, price, status]) =>
+                new Ticket(destination, Number(price), status)
+        )
+        .sort((a, b) => a[criteria].localeCompare(b[criteria]));
+
+    console.log(destinations);
+    return destinations;
+}
+
+tickets(
+    [
+        'Philadelphia|94.20|available',
+        'New York City|95.99|available',
+        'New York City|95.99|sold',
+        'Boston|126.20|departed',
+    ],
+    'destination'
+);
+
+tickets(
+    [
+        'Philadelphia|94.20|available',
+        'New York City|95.99|available',
+        'New York City|95.99|sold',
+        'Boston|126.20|departed',
+    ],
+    'status'
+);
