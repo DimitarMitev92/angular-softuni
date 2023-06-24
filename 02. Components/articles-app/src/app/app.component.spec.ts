d2bc5fe9440f55ac2c76f66fc2900de8361897b1
Dimitar Mitev <d.mitev920123@gmail.com>
2023-06-24 10:25:43 +0300

    01. Create Angular App

--- 02. Components/articles-app/src/app/app.component.spec.ts
+++ 02. Components/articles-app/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-second-app'`, () => {
+  it(`should have as title 'articles-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-second-app');
+    expect(app.title).toEqual('articles-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-second-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('articles-app app is running!');
   });
 });
