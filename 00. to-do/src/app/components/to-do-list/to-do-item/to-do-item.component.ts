487769eddaffde5547d941aad2820418befc11dc
Dimitar Mitev <d.mitev920123@gmail.com>
2023-06-28 19:44:10 +0300

    Check functionality

--- 00. to-do/src/app/components/to-do-list/to-do-item/to-do-item.component.ts
+++ 00. to-do/src/app/components/to-do-list/to-do-item/to-do-item.component.ts
@@ -6,10 +6,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./to-do-item.component.css'],
 })
 export class ToDoItemComponent {
+  isCheck: boolean = false;
   @Input() todo!: string;
   @Output() deletedTodoEvent = new EventEmitter<string>();
 
   addDeletedItem(value: string) {
     this.deletedTodoEvent.emit(value);
+  }
+
+  onCheck() {
+    this.isCheck = true;
+    console.log(this.isCheck);
   }
 }
